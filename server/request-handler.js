3ad30b8b37e055c1ff479b7d945fac0e5b16fa44
cheneym <cheneymao94@gmail.com>
2016-10-04 15:19:21 -0700

    Implement localhost to server app's index.html and other files

--- server/request-handler.js
+++ server/request-handler.js
@@ -31,6 +31,7 @@ var defaultCorsHeaders = {
 };
 
 var fs = require('fs');
+var path = require('path');
 
 var cache;
 
@@ -56,12 +57,60 @@ var requestHandler = function(request, response) {
   // Adding more logging to your server can be an easy way to get passive
   // debugging help, but you should always be careful about leaving stray
   // console.logs in your code.
+
+
   var headers = defaultCorsHeaders;
   headers['Content-Type'] = 'application/json';
   var statusCode = 200;
   var requestType = request.method;
 
-  if (request.url !== '/classes/messages') {
+
+
+  var url = request.url;
+  if (url === '/' || url.match(/client/) || url.match(/\?username=/)) {
+    var filePath = '.' + request.url;
+    if (filePath === './' || url.match(/\?username=/)) {
+      filePath = './client/index.html';
+    }
+    var extname = String(path.extname(filePath)).toLowerCase();
+    var contentType = 'text/html';
+    var mimeTypes = {
+      '.html': 'text/html',
+      '.js': 'text/javascript',
+      '.css': 'text/css',
+      '.json': 'application/json',
+      '.png': 'image/png',
+      '.jpg': 'image/jpg',
+      '.gif': 'image/gif',
+      '.wav': 'audio/wav',
+      '.mp4': 'video/mp4',
+      '.woff': 'application/font-woff',
+      '.ttf': 'applilcation/font-ttf',
+      '.eot': 'application/vnd.ms-fontobject',
+      '.otf': 'application/font-otf',
+      '.svg': 'application/image/svg+xml'
+    };
+
+    contentType = mimeTypes[extname] || 'application/octect-stream';
+
+    fs.readFile(filePath, function(error, content) {
+      if (error) {
+        if (error.code === 'ENOENT') {
+          fs.readFile('./404.html', function(error, content) {
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(content, 'utf-8');
+          });
+        } else {
+          response.writeHead(500);
+          response.end('Sorry, check with the site admin for error: ' + error.code + ' ..\n');
+          response.end();
+        }
+      } else {
+        response.writeHead(200, { 'Content-Type': contentType });
+        response.end(content, 'utf-8');
+      }
+    });
+  } else if (request.url !== '/classes/messages') {
     statusCode = 404;
     response.writeHead(statusCode, headers);
     response.end('Invalid URL');
@@ -72,6 +121,7 @@ var requestHandler = function(request, response) {
     response.end();
 
   } else if (requestType === 'GET') {
+
     statusCode = 200;
     response.writeHead(statusCode, headers);
     var data = {};
